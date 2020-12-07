502bcd6272ed9467de707999ade9f875c9a05fec
zbt-tools <l.feierabend@zbt-duisburg.de>
2020-12-07 17:49:32 +0100

    datetime implemented

--- data_import.py
+++ data_import.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import Frame
+from datetime import datetime
+
 
 def read_eis_data(file):
     # read out file data (experiement specs)
     # readout file data
 
     # read out experiment specifications
-    df_eis_specs = pd.read_csv(file, decimal=',', encoding='cp1252', error_bad_lines=False, delim_whitespace=True, index_col=False,
-                               header=None, nrows=7, keep_default_na=False)
+    df_eis_specs = pd.read_csv(file, decimal=',', encoding='cp1252',
+                               error_bad_lines=False, delim_whitespace=True,
+                               index_col=False, header=None, nrows=7,
+                               keep_default_na=False,
+                               names=['data', 'value#1', 'value#2', 'value#3'])
 
-    print(df_eis_specs)
     # read and format measurement data !fix with load, save, reload, columns --> to get into aspired design
     df_eis_data = pd.read_csv(file, sep='\t', decimal=',', encoding='cp1252', error_bad_lines=False, skiprows=19, index_col=False)
 
@@ -29,7 +33,25 @@ def read_eis_data(file):
     df_eis_data_formated.columns = ['index', 'number', 'frequency [Hz]', 'impedance Re [Ohm]', 'impedance Im [Ohm]',
                                     'significance', 'time [s]']
 
-    return df_eis_specs
+    print(df_eis_specs)
+    # filename
+    eis_specs_filename = df_eis_specs['value#1'].values[0]
+    # specification
+    eis_specs_spec = df_eis_specs['value#1'].values[1]
+    # date
+    eis_specs_date = df_eis_specs['value#3'].values[0]
+    # time
+    eis_specs_time = df_eis_specs['value#1'].values[5]
+    # voltage
+    eis_specs_voltage = df_eis_specs['value#1'].values[2]
+
+    eis_specs_date_time = eis_specs_date + ' ' + eis_specs_time
+    eis_specs_datetime = datetime.strptime(eis_specs_date_time,
+                                           '%b,%d.%Y %H:%M:%S')
+
+    return df_eis_specs, eis_specs_filename, eis_specs_spec, eis_specs_datetime, \
+           eis_specs_voltage
+
 
 def format_eis_data(file):
     # readout file data
@@ -74,26 +96,39 @@ def import_eis_data(frame, file):
     top_left_sampledoc_frame.grid(row=0, column=0)
     top_right_sampledoc_frame.grid(row=0, column=1)
 
+    df, filename, spec, datetime, voltage = read_eis_data(file)
 
 
     # labels of entries
         # labels (date, sample ,molarity, current, voltage)
     sdf_label1 = tk.Label(top_left_sampledoc_frame, text='Date:', pady=10)
     sdf_label2 = tk.Label(top_left_sampledoc_frame, text='Sample:', pady=10)
-    sdf_label3 = tk.Label(top_left_sampledoc_frame, text='Molarity [M]:', pady=10)
-    sdf_label4 = tk.Label(top_left_sampledoc_frame, text='Current [A]:', pady=10)
-    sdf_label5 = tk.Label(top_left_sampledoc_frame, text='Voltage [V]:', pady=10)
+    sdf_label3 = tk.Label(top_left_sampledoc_frame, text='Molarity [M]:',
+                          pady=10)
+    sdf_label4 = tk.Label(top_left_sampledoc_frame, text='Current [A]:',
+                          pady=10)
+    sdf_label5 = tk.Label(top_left_sampledoc_frame, text='Voltage [V]:',
+                          pady=10)
 
-    sdf_label6 = tk.Label(top_right_sampledoc_frame, justify='left', text=read_eis_data(file))
+    sdf_label6 = tk.Label(top_right_sampledoc_frame, justify='left', text=df)
+
+
+    sdf_label7 = tk.Label(bot_sampledoc_frame, text='File: ' + filename,
+                          pady=10)
+    sdf_label8 = tk.Label(bot_sampledoc_frame, text='Specs: ' + spec,
+                          pady=10)
 
         # label placement
-    sdf_label1.grid(row=1, column=0, sticky= 'ew')
-    sdf_label2.grid(row=2, column=0, sticky= 'ew')
-    sdf_label3.grid(row=3, column=0, sticky= 'ew')
-    sdf_label4.grid(row=4, column=0, sticky= 'ew')
-    sdf_label5.grid(row=5, column=0, sticky= 'ew')
+    sdf_label1.grid(row=1, column=0, sticky='ew')
+    sdf_label2.grid(row=2, column=0, sticky='ew')
+    sdf_label3.grid(row=3, column=0, sticky='ew')
+    sdf_label4.grid(row=4, column=0, sticky='ew')
+    sdf_label5.grid(row=5, column=0, sticky='ew')
 
-    sdf_label6.grid(row=1, column=0, sticky= 'e')
+    sdf_label6.grid(row=1, column=0, sticky='e')
+
+    sdf_label7.grid(row=1, column=0, sticky='ew')
+    sdf_label8.grid(row=1, column=1, sticky='ew')
 
 
 
@@ -101,7 +136,7 @@ def import_eis_data(frame, file):
 
         # entries (date, sample ,molarity, current, voltage)
     sdf_entry1 = tk.Entry(top_left_sampledoc_frame, width=30, bd=5)
-    sdf_entry1.insert(0, '')
+    sdf_entry1.insert(0, datetime)
     sdf_entry2 = tk.Entry(top_left_sampledoc_frame, width=30, bd=5)
     sdf_entry2.insert(0, '')
     sdf_entry3 = tk.Entry(top_left_sampledoc_frame, width=30, bd=5)
@@ -109,7 +144,7 @@ def import_eis_data(frame, file):
     sdf_entry4 = tk.Entry(top_left_sampledoc_frame, width=30, bd=5)
     sdf_entry4.insert(0, '')
     sdf_entry5 = tk.Entry(top_left_sampledoc_frame, width=30, bd=5)
-    sdf_entry5.insert(0, '')
+    sdf_entry5.insert(0, voltage)
 
         # entry placement
     sdf_entry1.grid(row=1, column=1)
--- skecthing.py
+++ skecthing.py
@@ -1,8 +1,12 @@
-import pandas as pd
+
+from datetime import datetime
+
+eis_specs_date = 'NOV,27.2020'
+eis_specs_time = '14:33:06'
+eis_specs_datetime = eis_specs_date + ' ' + eis_specs_time
+print(eis_specs_datetime)
 
 
+date_object = datetime.strptime(eis_specs_datetime, '%b,%d.%Y %H:%M:%S')
 
-print(df_eis_specs)
-print(df_eis_data)
-print(df_eis_data_formated)
-print(len(df_eis_data_formated.columns))
+print(date_object)
